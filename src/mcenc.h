7aff4051c370ee7be42f562c2e23e8101c36c686
Thomas Daede <daede003@umn.edu>
2015-06-09 15:21:49 -0700

    Double motion vector range

--- src/mcenc.h
+++ src/mcenc.h
@@ -29,7 +29,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.*/
 # define OD_MC_USE_CHROMA (1 << 0)
 
 /* The maximum search range for BMA. Also controls hit cache size. */
-#define OD_MC_SEARCH_RANGE (32)
+#define OD_MC_SEARCH_RANGE (64)
 
 typedef struct od_mv_node od_mv_node;
 typedef struct od_mv_dp_state od_mv_dp_state;
